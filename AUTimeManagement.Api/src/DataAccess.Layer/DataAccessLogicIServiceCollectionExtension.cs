6f9ed7995fb0c639af0e8ced2779f90127257877
DAbraham2 <daniel.abraham@edu.bme.hu>
2022-11-17 11:08:29 +0100

    updated ci to match new sonar settings

---
+++ AUTimeManagement.Api/src/DataAccess.Layer/DataAccessLogicIServiceCollectionExtension.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace AUTimeManagement.Api.DataAccess.Layer;
+
+public static class DataAccessLogicIServiceCollectionExtension
+{
+    public static IServiceCollection AddDataAccessLayer(this IServiceCollection services)
+    {
+        return services;
+    }
+
+    public static ISer
+}
+
